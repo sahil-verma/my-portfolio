cd4abf19f3ba5d2309a20cceca16f063a083daa5
7059705091 <verma.sahil3232@gmail.com>
2018-04-25 16:57:53 -0400

    initial commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AboutMeComponent } from './about-me/about-me.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { EducationComponent } from './education/education.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AboutMeComponent,
+    SkillsComponent,
+    WorkExperienceComponent,
+    ProjectsComponent,
+    EducationComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
